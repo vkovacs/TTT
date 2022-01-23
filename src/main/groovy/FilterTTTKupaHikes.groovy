c6c6bd9abeb8ad8a64abae68c307af4cd3d48f52
Viktor Kovacs <crusader8@gmail.com>
2022-01-23 15:55:52 +0100

    count budapest kupa hikes

--- src/main/groovy/FilterTTTKupaHikes.groovy
+++ src/main/groovy/FilterTTTKupaHikes.groovy
@@ -1,4 +1,20 @@
+import groovy.json.JsonSlurper
 
-boolean isTTTKupa(String details) {
-    details =~ /(?i).*Budapest kupa.*/ || details =~ /(?i).*TTT kupa.*/
+def inputFile = new File("../resources/all-hikes.json").readLines()
+JsonSlurper jsonSlurper = new JsonSlurper()
+
+def allHikes = jsonSlurper.parseText(inputFile[0]) as List
+final int BUDAPEST_REGION = 8
+final int BUDAI_HEGYSEG_REGION = 9
+
+println allHikes
+        .findAll { it.regions.contains(BUDAPEST_REGION) || it.regions.contains(BUDAI_HEGYSEG_REGION) }
+        .collect { new File("../resources/${it.id}").readLines()}
+        .collect {it.join()}
+        .findAll {isTTTKupa(it)}
+        .size()
+
+
+boolean isTTTKupa(String description) {
+    description =~ /(?i).*Budapest kupa.*/ || description =~ /(?i).*TTT kupa.*/
 }
