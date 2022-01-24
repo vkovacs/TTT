3fb75084cb00baac0446476f8185645737fd1332
Viktor Kovacs <viktor.kovacs@siemens.com>
2022-01-24 09:53:25 +0100

    test cannot be run from buildSrc

--- buildSrc/src/test/groovy/HikeHandlerTest.groovy
+++ buildSrc/src/test/groovy/HikeHandlerTest.groovy
@@ -1,8 +1,11 @@
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
 
 class HikeHandlerTest {
 private HikeHandler underTest = new HikeHandler()
 
-    //@Test
+    @Test
+    @Disabled("Project 'buildSrc' not found in root project 'TTT'")
     void timeFilter() {
         assert underTest.allHikesQueryUrl(2022) == "https://tturak.hu/api/hikeoccasion/list?from=1640995200&to=1672531199"
     }
