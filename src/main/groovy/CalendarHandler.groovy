c3369485ddfece04ef340d85e26bfeac729c6203
Viktor Kovacs <viktor.kovacs@siemens.com>
2022-01-26 13:49:48 +0100

    add calendar handler

---
+++ src/main/groovy/CalendarHandler.groovy
@@ -0,0 +1,5 @@
+class CalendarHandler {
+    void register(List<Hike> hikes) {
+        println(hikes)
+    }
+}
--- src/main/groovy/MyHikesHandler.groovy
+++ src/main/groovy/MyHikesHandler.groovy
@@ -7,7 +7,7 @@ import java.time.ZoneOffset
 @Field
 static final ZoneOffset myZoneOffset = ZoneOffset.of("+01:00")
 
-def inputFile = new File("../resources/all-hikes.json").readLines()
+def inputFile = new File("../../../datasource/all-hikes.json").readLines()
 JsonSlurper jsonSlurper = new JsonSlurper()
 
 def allHikes = jsonSlurper.parseText(inputFile[0]) as List
@@ -15,14 +15,16 @@ def allHikes = jsonSlurper.parseText(inputFile[0]) as List
 def myHikes = allHikes
         .findAll { it.regions.contains(Regions.BUDAPEST_REGION) || it.regions.contains(Regions.BUDAI_HEGYSEG_REGION) }
         .findAll {
-            def hikeDetails = new File("../resources/${it.id}").readLines().join()
+            def hikeDetails = new File("../../../datasource/${it.id}").readLines().join()
             isTTTKupa(hikeDetails)
         }
         .collect {
             new Hike(id: it.id, name: it.displayName, location: it.routes[0].route[0], startTime: LocalDateTime.ofEpochSecond(it.routes[0].startTimeFrom as long, 0, myZoneOffset), endTime: LocalDateTime.ofEpochSecond(it.routes[0].startTimeTo as long, 0, myZoneOffset),  url: "https://tturak.hu/hikeOccasion/${it.id}/details")
         }
 
-println(myHikes)
+new CalendarHandler().register(myHikes)
+
+
 
 boolean isTTTKupa(String description) {
     description =~ /(?i).*Budapest kupa.*/ || description =~ /(?i).*TTT kupa.*/
